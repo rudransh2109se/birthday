8ff6514ce9495e3fa32a48425da310614d2b1201
rudransh2109se <rudranshse21@gmail.com>
2026-01-20 08:37:09 +0530

    Update finalpage.js

--- finalpage.js
+++ finalpage.js
@@ -1,31 +1,34 @@
-alert("FINAL PAGE JS LOADED");
-// Balloon pop-up function
-function createBalloons(count) {
-  const container = document.getElementById("balloons-container");
+window.onload = function () {
 
-  for (let i = 0; i < count; i++) {
-    const balloon = document.createElement("div");
-    balloon.className = "balloon";
+  /* ===== BALLOONS ===== */
 
-    balloon.style.left = Math.random() * 90 + "vw";
-    balloon.style.width = "50px";
-    balloon.style.height = "70px";
-    balloon.style.backgroundImage = <balloon.png>;
-    balloon.style.backgroundSize = "contain";
-    balloon.style.backgroundRepeat = "no-repeat";
-    balloon.style.animationDuration = 4 + Math.random() * 3 + "s";
+  function createBalloons(count) {
+    const container = document.getElementById("balloons-container");
 
-    container.appendChild(balloon);
+    for (let i = 0; i < count; i++) {
+      const balloon = document.createElement("div");
+      balloon.className = "balloon";
 
-    balloon.addEventListener("animationend", () => balloon.remove());
+      balloon.style.left = Math.random() * 90 + "vw";
+      balloon.style.width = "50px";
+      balloon.style.height = "70px";
+      balloon.style.backgroundImage = 'url("balloon.png")';
+      balloon.style.backgroundSize = "contain";
+      balloon.style.backgroundRepeat = "no-repeat";
+      balloon.style.animationDuration = (4 + Math.random() * 3) + "s";
+
+      container.appendChild(balloon);
+
+      balloon.addEventListener("animationend", () => balloon.remove());
+    }
   }
-}
 
-window.onload = () => {
+  // start balloons
   createBalloons(20);
   setInterval(() => createBalloons(5), 3000);
-};
-window.onload = function () {
+
+
+  /* ===== HEARTS ===== */
 
   function createHeart() {
     const heart = document.createElement("div");
@@ -41,7 +44,7 @@ window.onload = function () {
     setTimeout(() => heart.remove(), 12000);
   }
 
+  // start hearts
   setInterval(createHeart, 500);
 
 };
-
